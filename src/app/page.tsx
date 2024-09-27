4170a1e64cde94af4503a27b9541341ef69e100f
Muhammad Ali <sheikhali12332@gmail.com>
2024-09-27 10:21:53 +0500

    9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,14 +36,14 @@ function HomePage() {
         <h2 className='text-[40px] font-semibold m-10'>Services I Offer</h2>
         <p className='text-justify font-normal text-[20px] m-10'>Legendary assassin John Wick (<a className='hover:text-[#3b83f6de]' href="https://en.wikipedia.org/wiki/Keanu_Reeves" target="_blank" rel="noopener noreferrer"> Keanu Reeves </a>) retired from his violent career aner marrying the love of his life. Her sudden death leaves John in deep mouning. When sadistic mobster Josef Tarasov (<a className='hover:text-[#3b83f6de]' href="https://en.wikipedia.org/wiki/Alfie_Allen" target="_blank" rel="noopener noreferrer">Alfie Allen</a>) and his thugs steal Johns prized car and kill the puppy that was a last gift from his wife. John unleashes die killing machine within and seeks vengeatre. Meanwhile Josephs father (<a className='hover:text-[#3b83f6de]' href="https://en.wikipedia.org/wiki/Michael_Nyqvist" target="_blank" rel="noopener noreferrer">Michael Nyqvist</a>) Johns former colleague puts a huge bounty on Johns head.</p>
 
-        {/* <div className='inline-flex  m-[30px] pl-[50px]'>
+        <div className='inline-flex  m-[30px] pl-[50px] md:m-1 md:pl-1'>
           <MyCard />
           <MyCard />
           <MyCard />
-        </div> */}
+        </div>
         
 
-        <div className='inline-flex m-[30px] pl-[50px] md:m-1 md:pl-1'>
+        {/* <div className='inline-flex m-[30px] pl-[50px] md:m-1 md:pl-1'>
           <div className='relative w-[25%] m-9 md:m-3 md:w-[35%] bg-white text-black rounded-md text-center hover:bg-[#3b83f6de] hover:text-black hover:translate-y-2 transition-transform duration-300'>
             <span className='pt-[20px]'><FaPencilRuler /></span>
             <p className='m-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eligendi obcaecati consequuntur optio, accusantium rerum, maiores nesciunt non debitis consectetur tenetur repellat iure, pariatur quia officiis aperiam recusandae ipsum praesentium!</p>
@@ -59,7 +59,7 @@ function HomePage() {
             <p className='m-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eligendi obcaecati consequuntur optio, accusantium rerum, maiores nesciunt non debitis consectetur tenetur repellat iure, pariatur quia officiis aperiam recusandae ipsum praesentium!</p>
             <p className='m-6 font-semibold'>Dumy text</p>
           </div>
-        </div>
+        </div> */}
         
         <div className='inline-flex mr-[60px] ml-[60px] md:mr-1 md:ml-1'>
           <Image className=' ml-[150px] mr-[50px] md:ml-3 md:mr-4'  src="https://i.pinimg.com/236x/5a/13/6e/5a136e37fb15c735f795a5df4a87fdcb.jpg" alt="" width={500} height={100}/>
--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function MyCard()  {
   return (
-    <div className='relative w-[25%] m-9 bg-white text-black rounded-md text-center hover:bg-[#3b83f6de] hover:text-black hover:translate-y-2 transition-transform duration-300'>
-      <span className='pt-[20px]'>1</span>
+    <div className='relative w-[25%] md:w-[35%] m-9 md:m-3 bg-white text-black rounded-md text-center hover:bg-[#3b83f6de] hover:text-black hover:translate-y-2 transition-transform duration-300'>
+      <span>2</span>
       <p className='m-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eligendi obcaecati consequuntur optio, accusantium rerum, maiores nesciunt non debitis consectetur tenetur repellat iure, pariatur quia officiis aperiam recusandae ipsum praesentium!</p>
       <p className='m-6 font-semibold'>Dumy text</p>
     </div>
